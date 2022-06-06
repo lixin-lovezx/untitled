eff98edbb3ecc44de43b5113646ed85f046e7e9d
lixin <lx2017v@163.com>
2022-06-06 20:56:59 +0800

    hot-fix commit1

--- src/main/java/com/lixin/git/GitTest.java
+++ src/main/java/com/lixin/git/GitTest.java
@@ -1,9 +1,13 @@
 package com.lixin.git;
 
+/**
+ * @author asus
+ */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("123 123");
         System.out.println("123 123 123");
+        System.out.println("123 123 123 123");
     }
 }
